999c0801d97c09414e50f3964180fa9d7b6689df
lazarskim <lazarskim@zsi.kielce.pl>
2022-04-06 09:28:29 +0200

    master

---
+++ index.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+ <title>Formularz zamówień</h1>
+</head>
+<body>
+<!-- formularz do obsługi skryptu-->
+<h1>Formularz zamówień</h1>
+<br>
+<h3>Osoba kontaktowa</h3><br>
+<input type="text" name="osobakontaktowa"/><br>
+<input type="number" name="telefon"/><br>
+<h3>Zamów samochód</h3>
+<input type="text" name="model"/><br>
+<input type="text" name="marka"/><br>
+<input type="date" name="rocznik"/><br>
+<input type="number" name="przebieg"/><br>
+<selection name="kolor">
+ <option>niebieski</option>
+ <option>czerwony</option>
+ <option>zielony</option>
+ <option>pomaranczowy</option>
+</selection><br>
+<textarea type="text" name="notatki">dodatkowanie informacje</textarea><br>
+<input type="submit" value="zamow">
+<input type="reset" value="wyszysc">
+<?php
+//skrypt PHP
+
+
+
+?>
+
+</body>
+</html>
